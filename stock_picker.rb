90bed1ba5d7c9e45636963919ed5839d7bb92cf4
Peter <petertran198@gmail.com>
2020-04-09 15:32:24 -0400

    Added edge case

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,5 +1,5 @@
 # #Implement a method stock_picker that takes in an array of stock prices, one for each hypothetical day. It should return a pair of days representing the best day to buy and the best day to sell. Days start at 0.
-
+#Pay attention to edge cases like when the lowest day is the last day or the highest day is the first day.
 
 
 def stock_picker(stock_arr)
