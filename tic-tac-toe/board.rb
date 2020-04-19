17b4717aa17580c132b7ae2f6dc045b5eec76c6e
Peter <petertran198@gmail.com>
2020-04-19 11:40:02 -0400

    Fixed error where the loop did not break if someone won until the end

--- tic-tac-toe/board.rb
+++ tic-tac-toe/board.rb
@@ -7,20 +7,24 @@ class Board
           7, 8, 9
         ]
     end
-  
+
     def show
-     puts " "
-     puts "#{@field[0]} | #{@field[1]} | #{@field[2]}"
-     puts "#{@field[3]} | #{@field[4]} | #{@field[5]}"
-     puts "#{@field[6]} | #{@field[7]} | #{@field[8]}"
+      puts " "
+      puts "#{@field[0]} | #{@field[1]} | #{@field[2]}"
+      puts "#{@field[3]} | #{@field[4]} | #{@field[5]}"
+      puts "#{@field[6]} | #{@field[7]} | #{@field[8]}"
     end
-  
+
+    #check if all the field is tied by either player1's symbol or player2's symbol 
+    #Return false if not full,  true if it is 
     def full?(player1, player2) 
-        @field.all? {|f| f == player1.symbol || f == player2.symbol }
+        full = @field.all? {|f| f == player1.symbol || f == player2.symbol }
+        full ?  "-------Board full, no one won----------" : ""
+        return full
     end
-  
-  
-  
-  end
+
+end
+
+
   
   
