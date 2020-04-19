6d0b480fc2d4124ab2141ad9c2defdc9e2a4fe87
Peter <petertran198@gmail.com>
2020-04-19 16:26:34 -0400

    Fixed bug that did not reset value  when played a new game

--- tic-tac-toe/board.rb
+++ tic-tac-toe/board.rb
@@ -1,27 +1,29 @@
 class Board 
-    attr_accessor :field
-    def initialize(field)
-      @field = field
-    end
+  attr_accessor :field
+  def initialize()
+    @field = [
+      1, 2, 3,
+      4, 5, 6,
+      7, 8, 9
+    ]
+  end
 
-    def show
-      i = 0
-      puts " "
-      while @field.length > i 
-        puts "#{@field[i]} | #{@field[i+1]} | #{@field[i+2]}"
-        i += 3
-      end
-    end
+  def show
+    puts " "
+    puts "#{@field[0]} | #{@field[1]} | #{@field[2]}"
+    puts "#{@field[3]} | #{@field[4]} | #{@field[5]}"
+    puts "#{@field[6]} | #{@field[7]} | #{@field[8]}"
+  end
 
-    #check if all the field is tied by either player1's symbol or player2's symbol 
-    #Return false if not full,  true if it is 
-    def full?(player1, player2) 
-        full = @field.all? {|f| f == player1.symbol || f == player2.symbol }
-        full ?  "-------Board full, no one won----------" : ""
-        return full
-    end
+  #check if all the field is tied by either player1's symbol or player2's symbol 
+  #Return false if not full,  true if it is 
+  def full?(player1, player2) 
+      full = @field.all? {|f| f == player1.symbol || f == player2.symbol }
+      return full
+  end
 
 end
 
 
-  
+
+
